1c5ef195000be671b3e6390f69d49d6f706ad05d
Alona <alenija23@gmail.com>
2015-12-05 16:43:24 +0200

    modif

--- books.php
+++ books.php
@@ -11,5 +11,18 @@ class Books_model extends Model{
 
 	function add_one($array){
 
+		$query = "INSERT INTO `$this->_table` SET `$this->_fields_aut` = ?";
+    	$stmt = mysqli_prepare($this->_c, $query);
+    	//print_r($this->_c);
+    	if ($stmt) {
+    		$count = count($data_add);
+            for ($i=0; $i < $count; $i++) {
+                mysqli_stmt_bind_param($stmt, 's', $data_add[$i]);
+                mysqli_stmt_execute($stmt);
+            }
+        }
+        return printf("Rows inserted: %d", $count);  //mysqli_stmt_affected_rows($stmt));
+	    mysqli_stmt_close($stmt);
+
 	}
 }
